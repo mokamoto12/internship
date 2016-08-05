b64f42b8a377b53b4880c8fcaeb0182e44cb3d14
mokamoto12 <okamoto_masahiro@aratana.jp>
2016-08-05 10:00:30 +0900

    Fix adjust chrome

--- dev/js/controller.js
+++ dev/js/controller.js
@@ -17,3 +17,19 @@ document.onkeypress = function (e) {
     render();
   }
 };
+document.body.onkeydown = function (e) {
+  // キーに名前をセットする
+  var keys = {
+    37: 'left',
+    39: 'right',
+    40: 'down',
+    38: 'rotate'
+  };
+
+  if (typeof keys[e.keyCode] != 'undefined') {
+    // セットされたキーの場合はtetris.jsに記述された処理を呼び出す
+    keyPress(keys[e.keyCode]);
+    // 描画処理を行う
+    render();
+  }
+};
